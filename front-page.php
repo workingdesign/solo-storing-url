b5fc57d128ff737df9ea276986f283359d62222b
Giorgio Riccardi <griccardi@my.bcit.ca>
2015-03-19 09:54:57 -0700

    debug <input url>

---
+++ front-page.php
@@ -0,0 +1,174 @@
+<?php
+
+// Exit if accessed directly
+if ( !defined('ABSPATH')) exit;
+
+/**
+ * Site Front Page
+ *
+ * Note: You can overwrite front-page.php as well as any other Template in Child Theme.
+ * Create the same file (name) include in /responsive-child-theme/ and you're all set to go!
+ * @see            http://codex.wordpress.org/Child_Themes and
+ *                 http://themeid.com/forum/topic/505/child-theme-example/
+ *
+ * @file           front-page.php
+ * @package        Soul 
+ * @author         Fred Chevalier - Workingdesign 
+ * @version        Release: 1.0
+ * @since          available since Release 1.0
+ */
+
+/**
+ * Globalize Theme Options
+ */
+$responsive_options = responsive_get_options();
+/**
+ * If front page is set to display the
+ * blog posts index, include home.php;
+ * otherwise, display static front page
+ * content
+ */
+if ( 'posts' == get_option( 'show_on_front' ) && $responsive_options['front_page'] != 1 ) {
+	get_template_part( 'home' );
+} elseif ( 'page' == get_option( 'show_on_front' ) && $responsive_options['front_page'] != 1 ) {
+	$template = get_post_meta( get_option( 'page_on_front' ), '_wp_page_template', true );
+	$template = ( $template == 'default' ) ? 'index.php' : $template;
+	locate_template( $template, true );
+} else { 
+
+	get_header();
+	
+	//test for first install no database
+	$db = get_option( 'responsive_theme_options' );
+    //test if all options are empty so we can display default text if they are
+    $empty = ( empty( $responsive_options['home_headline'] ) && empty( $responsive_options['home_subheadline'] ) && empty( $responsive_options['home_content_area'] ) ) ? false : true;
+	?>
+
+	<!-- slider -->
+	<div class="flexslider">
+
+		<ul class="slides">
+		<?php 
+			$loop = new WP_Query(array('post_type' => 'slides', 'posts_per_page' => -1, 'order'=> 'ASC')); 
+			
+			while ( $loop->have_posts() ) : $loop->the_post(); ?>
+
+				<li>
+					<?php $url = get_post_meta($post->ID, "url", true);
+					if($url!='') { 
+						echo '<a href="'.$url.'">';
+						echo the_post_thumbnail('full');
+						echo '</a>';
+					} else {
+						echo the_post_thumbnail('full');
+					} ?>
+							
+				</li>
+
+				<?php endwhile; ?>
+					
+				<?php wp_reset_query(); ?>
+			</ul>
+
+	</div>
+
+	<div id="featured" class="grid col-940">
+	
+		<div id="featured-intro" class="grid col-540">
+			<div class="featured-wrapper">
+			<?php
+				$feature = new WP_Query();
+                $feature->query(array( 'page_id' => 33 ));
+                while ( $feature->have_posts()) : $feature->the_post();?>
+                
+                <div class="featured-content">
+                	<?php the_content();?>
+                </div>
+                <?php endwhile;
+
+            wp_reset_query(); ?>
+			</div><!-- end of featured-wrapper -->
+			
+		</div><!-- end of .col-540 -->
+
+		<div id="featured-events" class="grid col-380 fit">
+			<div class="featured-wrapper">
+
+				<h3>Upcoming Events</h3>
+		  
+				<?php
+				$args = array( 
+					'post_type' => 'shows',
+		        	'orderby' => 'show_date',
+		        	'meta_query' => array(
+		            	array(
+		               		'key' => 'show_date',
+		                	'value' => current_time( 'timestamp' ), //grc 11-2014
+		                	//'value' => current_time( 'mysql' ), // reverted 03/2015 grc
+		                	'type' => 'NUMERIC',
+		                	'compare' => '>='
+		            ),
+		        ),
+		        	'orderby' => 'meta_value_num',
+	        		'order' => 'ASC',
+	        		//'posts_per_page' => 2, //grc 11-2014
+	        		'posts_per_page' => 1,
+		        );
+
+				 $loop = new WP_Query( $args );
+				   while ( $loop->have_posts() ) : $loop->the_post();?>
+				    
+				    <div class="information-event">
+				    	
+				    	<div class="thumbnail-event"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_post_thumbnail('thumbnail'); ?></a>
+				    	</div>
+
+				    	<h4>
+				    		<a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a>
+				    		<br />by <?php echo get_post_meta($post->ID, 'show_author', true) ?>
+				    	</h4>
+
+				    	<p>
+					    	<?php 
+					    		$unixtime = get_post_meta($post->ID, 'show_date', true);				    		
+					    		echo get_post_meta($post->ID, 'show_start_date', true);
+					    		echo " to ";
+					    		echo date("F d, Y", $unixtime);
+					    	?>
+					    	<br />
+					    	<?php echo get_post_meta($post->ID, 'show_venue', true); ?>
+					    	<br />
+					    	<a href="<?php get_post_meta($post->ID, 'show_url', true); ?>" target="_blank">
+					    		<?php echo get_post_meta($post->ID, 'show_url', true); ?>
+					    	</a>
+					    	<br />
+					    	<br>
+					    	<?php echo get_post_meta($post->ID, 'show_tel', true); ?>
+					    	<br />
+					    	<?php echo get_post_meta($post->ID, 'show_price', true); ?>
+					    	<br />
+					    	<?php echo get_post_meta($post->ID, 'show_featuring', true); ?>
+				    	</p>
+
+					</div>
+
+					<!-- <div class="thumbnail-event"><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_post_thumbnail('thumbnail'); ?></a></div> -->
+
+				    <div class="clear"></div>
+
+
+				<?php endwhile; 
+				wp_reset_query(); ?>
+			  
+			</div><!-- end of featured-wrapper -->						
+		</div><!-- end of #featured-events --> 
+
+		<div class="clear"></div>
+	
+	</div><!-- end of #featured -->
+               
+	<?php 
+	get_sidebar('home');
+	get_footer(); 
+}
+?>
---
+++ show.php
@@ -0,0 +1,255 @@
+<?php
+/**
+ * Adds Custom Post types Shows .
+ */
+add_action('init', 'soul_create_shows');
+function soul_create_shows() {
+
+	$labels = array(
+		'name' => _x('SHOWS', 'post type general name'),
+		'singular_name' => _x('show', 'post type singular name'),
+		'add_new' => _x('Add New', 'show'),
+		'add_new_item' => __('Add New show'),
+		'edit_item' => __('Edit show'),
+		'new_item' => __('New show'),
+		'view_item' => __('View show'),
+		'search_items' => __('Search shows'),
+		'not_found' =>  __('Nothing found'),
+		'not_found_in_trash' => __('Nothing found in Trash'),
+		'parent_item_colon' => ''
+	);
+
+	$args = array(
+		'labels' => $labels,
+		'public' => true,
+		'publicly_queryable' => true,
+		'show_ui' => true,
+		'query_var' => true,
+		'rewrite' => true,
+		'capability_type' => 'post',
+		'hierarchical' => false,
+		'menu_position' => null,
+        'menu_icon' => 'dashicons-editor-video',
+		'supports' => array('title','editor','excerpt','thumbnail')
+	  );
+
+	register_post_type( 'shows' , $args );
+}
+
+// Add show type taxonomy for sticky option
+
+add_action( 'init', 'soul_register_taxonomy_shows' );
+
+function soul_register_taxonomy_shows() {
+
+    $labels = array( 
+        'name' => _x( 'show_types', 'show_types' ),
+        'singular_name' => _x( 'show_type', 'show_types' ),
+        'search_items' => _x( 'Search show_types', 'show_types' ),
+        'popular_items' => _x( 'Popular show_types', 'show_types' ),
+        'all_items' => _x( 'All show_types', 'show_types' ),
+        'parent_item' => _x( 'Parent show_type', 'show_types' ),
+        'parent_item_colon' => _x( 'Parent show_type:', 'show_types' ),
+        'edit_item' => _x( 'Edit show_type', 'show_types' ),
+        'update_item' => _x( 'Update show_type', 'show_types' ),
+        'add_new_item' => _x( 'Add New show_type', 'show_types' ),
+        'new_item_name' => _x( 'New show_type', 'show_types' ),
+        'separate_items_with_commas' => _x( 'Separate show_types with commas', 'show_types' ),
+        'add_or_remove_items' => _x( 'Add or remove show_types', 'show_types' ),
+        'choose_from_most_used' => _x( 'Choose from the most used show_types', 'show_types' ),
+        'menu_name' => _x( 'show_types', 'show_types' ),
+    );
+
+    $args = array( 
+        'labels' => $labels,
+        'public' => true,
+        'show_in_nav_menus' => false,
+        'show_ui' => true,
+        'show_tagcloud' => true,
+        'show_admin_column' => true,
+        'hierarchical' => true,
+        'rewrite' => true,
+        'query_var' => true
+    );
+
+    register_taxonomy( 'show_types', array('shows'), $args );
+}
+
+/* Display list shows */
+add_action("manage_posts_custom_column",  "soul_shows_custom_columns");
+add_filter("manage_shows_posts_columns", "soul_shows_edit_columns");
+
+function soul_shows_edit_columns($columns){
+    $columns = array(
+        "cb" => "<input type=\"checkbox\" />",
+        "title" => "Show",
+        "show_author" => "Author",
+        "show_date" => "Date",
+        "show_venue" => "Venue"
+  );
+  return $columns;
+}
+
+function soul_shows_custom_columns($column){
+    global $post;
+    $custom = get_post_custom();
+
+    switch ($column) {
+    case "show_date":
+            echo soul_format_date($custom["show_date"][0]);
+            break;
+
+    //grc 03/2015
+    case "show_start_date":
+            echo $custom["show_start_date"][0];
+            break;
+
+    case "show_venue":
+            echo $custom["show_venue"][0];
+            break;
+
+	case "show_url":
+            echo $custom["show_url"][0];
+            break;
+
+    case "show_tel":
+            echo $custom["show_tel"][0];
+            break;
+
+    case "show_price":
+            echo $custom["show_price"][0];
+            break;
+
+    case "show_featuring":
+            echo $custom["show_featuring"][0];
+            break;
+
+    case "show_author":
+            echo $custom["show_author"][0];
+            break;
+
+    default:
+       echo "error";
+    }
+}
+
+/* Add sortable shows column */
+add_filter("manage_edit-shows_sortable_columns", "soul_show_date_column_register_sortable");
+add_filter("request", "soul_show_date_column_orderby" );
+
+function soul_show_date_column_register_sortable( $columns ) {
+        $columns['show_date'] = 'show_date';
+        return $columns;
+}
+
+function soul_show_date_column_orderby( $vars ) {
+    if ( isset( $vars['orderby'] ) && 'show_date' == $vars['orderby'] ) {
+        $vars = array_merge( $vars, array(
+            'meta_key' => 'show_date',
+            'orderby' => 'meta_value_num'
+        ) );
+    }
+    return $vars;
+}
+
+/* add taxonomy column */
+// add_filter( 'manage_taxonomies_for_shows_columns', 'show_types_columns' );
+// function show_types_columns( $taxonomies ) {
+//     $taxonomies[] = 'show-types';
+//     return $taxonomies;
+// }
+
+/* Add shows details metabox */
+add_action("admin_init", "soul_shows_admin_init");
+
+function soul_shows_admin_init(){
+  add_meta_box("show_meta", "show Details", "soul_show_details_meta", "shows", "normal", "default");
+}
+
+function soul_format_date($unixtime) { 
+    return date("F", $unixtime)." ".date("d", $unixtime).", ".date("Y", $unixtime); 
+}
+
+function soul_show_details_meta() {
+
+    // echo '<p><strong>AFTER EDITING A FIELD, ALWAYS PASTE AGAIN THE URL STRUCTURE:</strong>
+    //         <br /> &lt;a href="http://www.my-site.com/" target"_blank"&gt;My Site Title&lt;/a&gt;
+    //     </p>';
+
+	$ret = '<p><label>Author: </label><input type="text" size="70" name="show_author" value="' . soul_get_show_field("show_author") . '" /></p>';
+	$ret = $ret . '<p><label>Start Date: </label><input type="text" size="30" name="show_start_date" value="' . soul_get_show_field("show_start_date") . '" /></p>';
+    $ret = $ret . '<p><label>End Date: </label><input type="text" name="show_date" value="' . soul_format_date(soul_get_show_field("show_date")) . '" /><em> (mm/dd/yyyy)</em></p>';
+    $ret = $ret . '<p><label>Venue: </label><input type="text" size="70" name="show_venue" value="' . soul_get_show_field("show_venue") . '" /></p>';
+
+    echo "<br>";
+
+// GRC test for custom field in the post
+    $ret = $ret . '<p><label>Url [<em>starts with <b>http://</b></em>]: </label><input type="url" size="70" name="show_url" value="' . soul_get_show_field("show_url") . '" /><em> http://your-site.com</em></p>';    
+    $ret = $ret . '<p><label>Phone Number: </label><input type="text" size="30" name="show_tel" value="' . soul_get_show_field("show_tel") . '" /></p>';
+    $ret = $ret . '<p><label>Ticket Price: </label><input type="text" size="100" name="show_price" value="' . soul_get_show_field("show_price") . '" /></p>';
+    $ret = $ret . '<p><label>Featuring: </label><input type="text" size="120" name="show_featuring" value="' . soul_get_show_field("show_featuring") . '" /></p>';
+
+    // $ret = $ret . '<p><label>Featuring: </label><input type="text" size="120" name="show_featuring" value="' . soul_get_show_field("show_featuring") . '" /></p>';
+    // $ret = $ret . '<p><label>Featuring: </label><textarea rows="4" cols="50" name="show_featuring" value="' . soul_get_show_field("show_featuring") . '" ></textarea></p>';
+
+	echo $ret;
+}
+
+function soul_get_show_field($show_field) {
+    global $post;
+
+    $custom = get_post_custom($post->ID);
+
+    if (isset($custom[$show_field])) {
+        return $custom[$show_field][0];
+    }
+}
+
+/* Add saving shows details */
+add_action('save_post', 'soul_save_show_details');
+
+function soul_save_show_details(){
+   global $post;
+
+   if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+      return;
+
+   if ( get_post_type($post) == 'show')
+      return;
+
+   if(isset($_POST["show_date"])) {
+      update_post_meta($post->ID, "show_date", strtotime($_POST["show_date"]));
+   }
+
+   soul_save_show_field("show_author");
+   soul_save_show_field("show_venue");
+   soul_save_show_field("show_start_date"); //grc 03/2015
+   soul_save_show_field("show_url");
+   soul_save_show_field("show_tel");
+   soul_save_show_field("show_price");
+   soul_save_show_field("show_featuring");
+}
+
+function soul_save_show_field($show_field) {
+    global $post;
+
+    if(isset($_POST[$show_field])) {
+        update_post_meta($post->ID, $show_field, $_POST[$show_field]);
+    }
+}
+
+/* function to get show details */
+function soul_get_show_details($include_register_button, $include_title) {
+    global $post;
+    $unixtime = get_post_meta($post->ID, 'show_date', true);
+
+    $ret = '';
+    if ($include_title) {
+        $ret =  $ret . '<h3><a href="' . get_permalink() . '">' . $post->post_title . '</a></h3>';
+    }
+    $ret = $ret . '<p><strong>Author: </strong>' . get_post_meta($post->ID, 'show_author', true) . ' - ';
+    $ret = $ret . '<strong>Venue: </strong>'.get_post_meta($post->ID, 'show_venue', true) . '</p>';
+
+    return $ret;
+}
+
